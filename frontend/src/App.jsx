f6ac304c33044d1116dae8301f69908df53eccb0
Sabján Patrik <SabPat702@hengersor.hu>
2025-02-25 12:25:18 +0100

    finish this

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'tachyons/css/tachyons.min.css'
+import { Home } from './components/pages/Home'
+
+function App() {
+  
+
+  return (
+  <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+  </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,53 @@
+import {React} from 'react'
+export const Home = () => {
+
+
+return(
+    <>
+        <div>
+            <div>
+                <img src="top.jpg" alt="Táj kép" />
+            </div>
+        </div>
+        <div>
+            <div>
+                    <div>
+                        <h3>Napraforgós Nemzeti Tanúsító Védjegy célja</h3>
+                        <br />
+                        <p>
+                            A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma. A védjegy alapvető célja, hogy – összhangban az egyes szálláshelyek működtetéséről szóló 239/2009. Korm. rendeletben foglaltakkal – garanciát nyújtson a szálláshely szolgáltatás minőségének megfelelő színvonalára.  A falusi vendégházak 1-4 napraforgós besorolást nyerhetnek el a külső, belső megjelenés, a felszereltség, a szolgáltatások színvonala, valamint a szállásadó személyes felkészültségének, szakmai képzettségének függvényében. 
+                        </p>
+                        <a href="https://falusiturizmus.eu/"></a>
+                        <img src="logo.png" alt="A falusi túrizmus logója" />
+                        <br />
+                        <img src="holloko_masolata.jpg" alt="Hollókő másolata" />
+                    </div>
+                    <div>
+                        <h3>Falusi szálláshely fajtái</h3>
+                        <li>Vendégszoba: a vendégek rendelkezésére bocsátható önálló lakóegység, amely egy lakóhelyiségből, és a minősítéstől függően a hozzátartozó mellékhelyiségekből áll.</li>
+                        <li>Lakrész: önálló épület kettő, illetve több szobából álló lehatárolt része a minősítéstől függően hozzátartozó mellékhelyiségekkel együtt</li>
+                        <li>Vendégház: önálló épület, több szobával, mellékhelyiségekkel és főzési lehetőséggel rendelkező lakó-, illetve üdülőegység, családok vagy kisebb csoportok elszállásolására.</li>
+                        <li>Sátorozóhely: csak valamelyik falusi szálláshely típus mellett, mintegy azt kiegészítve üzemeltethető az előírt feltételek megléte esetén. Pl.: falusi vendégház sátorozóhellyel.</li>
+                        <img src="ketagyas.jpg" alt="Kétágyas szoba" />
+                    </div>
+                    <div>
+                    <h3>A hét törpe fogadó</h3>
+                    <li>Ruhásszekrény</li>
+                    <li>Saját fürdőszoba zuhanytálca</li>
+                    <li>WC (fürdőszobával egyben)</li>
+                    </div>
+            </div>
+        </div>
+        <div>
+                <div>
+                    <div>
+                        <h3>A vendégszobák foglalatsága</h3>
+                    </div>
+                    <div>
+                        <h3>A szobák kihasználtsága:</h3>
+                    </div>
+                </div>
+        </div>
+    </>
+    )
+}
